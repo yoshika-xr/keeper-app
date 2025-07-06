9d1abe99004410ccd33ae3554f955b19ad1d917c
yoshika-xr <yoshikajune@gmail.com>
2025-07-06 20:13:42 +0530

    add some contents

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,7 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-//Challenge. Render all the notes inside notes.js as a seperate Note
-//component.
+
 
 // If you're running this locally in VS Code use the commands:
 // npm install
