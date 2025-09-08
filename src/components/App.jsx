73383bcb996d12b5b9e3e58eff7eef899229f603
yoshika-xr <yoshikajune@gmail.com>
2025-09-08 22:23:16 +0530

    add form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React,{useState}from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes,setNotes]=useState([]);
+
+  function AddNote(newNote){
+    setNotes(prevNotes=>{
+      return[...prevNotes,newNote]
+    })
+    console.log(newNote)
+  }
   return (
-    <div>
+    <div> 
       <Header />
-      {notes.map(noteItem => (
-        <Note
-          key={noteItem.key}
-          title={noteItem.title}
-          content={noteItem.content}
-        />
-      ))}
+      <CreateArea 
+      onAdd={AddNote}/>
+     { notes.map(noteItem => {
+       return <Note title={noteItem.title}
+      content={noteItem.content} />
+      })}
+      <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
   );
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,44 @@
+import React,{useState} from "react";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  };
+
+  function SubmitNote(event){
+    props.onAdd(note)
+    event.preventDefault();
+  }
+  return (
+    <div>
+      <form>
+        <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note....."
+          row="3"
+        />
+        <button  onClick={SubmitNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+export default CreateArea;
